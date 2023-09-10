8710870e8ff8721823d307b23805704844832897
Bruno Augusto <bruno_agst@hotmail.com>
2023-09-10 16:11:34 -0300

    update privileges

--- include/EEPROM.hpp
+++ include/EEPROM.hpp
@@ -6,10 +6,7 @@
 class eeprom {
     public:
         eeprom(const int CLK, const int LATCH, const int DS_DATA, const int OE, const int WE, const int IO0, const int IO1, const int IO2, const int IO3, const int IO4, const int IO5, const int IO6, const int IO7);
-        void setAddress(int8_t hex);
-        void configureControlPins(void);
-        void configureEEPROMRead(void);
-        void configureEEPROMWrite(void);
+        void configureControlPins(void);      
         void readEEPROM(int8_t address);
         void writeEEPROM(int8_t address, byte data);
         
@@ -27,6 +24,10 @@ class eeprom {
         int _IO5;
         int _IO6;
         int _IO7;
+        void configureEEPROMRead(void);
+        void configureEEPROMWrite(void);
+        void setAddress(int8_t hex);
+
 };
 
 #endif
